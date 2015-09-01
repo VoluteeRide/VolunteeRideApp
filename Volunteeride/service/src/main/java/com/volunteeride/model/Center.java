02e84ba94ae02107ed5141f8406b0abd32ab5f4e
Ayaz Lakdawala <ayazlakdawala@gmail.com>
2015-09-01 00:48:01 -0400

    #7 Added skeleton Service, ServiceImpl, DAO, DAOImpl and model objects for request ride api

---
+++ Volunteeride/service/src/main/java/com/volunteeride/model/Center.java
@@ -0,0 +1,30 @@
+package com.volunteeride.model;
+
+import java.util.List;
+
+/**
+ * Model class representing religious center
+ *
+ * Created by ayazlakdawala on 9/1/15.
+ */
+public class Center {
+
+    private Location location;
+    private List<Location> pickUpLocations;
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public List<Location> getPickUpLocations() {
+        return pickUpLocations;
+    }
+
+    public void setPickUpLocations(List<Location> pickUpLocations) {
+        this.pickUpLocations = pickUpLocations;
+    }
+}
