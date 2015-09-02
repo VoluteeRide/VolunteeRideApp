55658c2613c778856063cd276df4b95abcc513e4
Ayaz Lakdawala <ayazlakdawala@gmail.com>
2015-09-02 01:12:36 -0400

    #8 Added spock dependency and a sample skeleton spock test.

---
+++ Volunteeride/service/src/test/groovy/com/volunteeride/service/impl/VolunteeRideServiceImplTest.groovy
@@ -0,0 +1,34 @@
+package com.volunteeride.service.impl
+
+import com.volunteeride.model.Center
+import com.volunteeride.model.Location
+import com.volunteeride.model.Ride
+import spock.lang.Shared
+import spock.lang.Specification
+
+/**
+ * Created by ayazlakdawala on 9/1/15.
+ */
+class VolunteeRideServiceImplTest extends Specification {
+
+    def ride = new Ride();
+    def volunteerideService = new VolunteeRideServiceImpl();
+
+    @Shared
+    def center = new Center();
+
+    void setup() {
+
+    }
+
+    void cleanup() {
+
+    }
+
+    def "validate ride object for Request Ride api "() {
+
+        expect:
+        volunteerideService.requestRide(ride)== null;
+
+    }
+}
