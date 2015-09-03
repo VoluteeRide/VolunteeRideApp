a1a2ee8b3f88c6ce42a1c342c1ec08dbab72d57c
ayazlakdawala <ayazlakdawala@gmail.com>
2015-09-03 18:38:29 -0400

    Revert "Revert "#6 removed .gradle files""
    This reverts commit 3bccf81c082ce12ae87697dc9a0920939fb801fd.

--- Volunteeride/service/src/test/groovy/com/volunteeride/service/impl/VolunteeRideServiceImplTest.groovy
+++ Volunteeride/service/src/test/groovy/com/volunteeride/service/impl/VolunteeRideServiceImplTest.groovy
@@ -1,7 +1,6 @@
 package com.volunteeride.service.impl
 
 import com.volunteeride.model.Center
-import com.volunteeride.model.Location
 import com.volunteeride.model.Ride
 import spock.lang.Shared
 import spock.lang.Specification
@@ -9,13 +8,13 @@ import spock.lang.Specification
 /**
  * Created by ayazlakdawala on 9/1/15.
  */
-class VolunteeRideServiceImplTest extends Specification {
+class VolunteeRideServiceImplTest extends Specification  {
 
     def ride = new Ride();
     def volunteerideService = new VolunteeRideServiceImpl();
 
     @Shared
-    def center = new Center();
+     center = new Center();
 
     void setup() {
 
