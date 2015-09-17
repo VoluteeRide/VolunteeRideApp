1418fb593dddb922e85a65bd8cf4ee653a84c50c
ayazlakdawala <ayazlakdawala@gmail.com>
2015-09-17 18:47:33 -0400

    #7 Refactored base model object to have spring data compliant Id and Version fields

--- Volunteeride/service/src/main/java/com/volunteeride/model/BaseModelObject.java
+++ Volunteeride/service/src/main/java/com/volunteeride/model/BaseModelObject.java
@@ -1,22 +1,36 @@
 package com.volunteeride.model;
 
 import org.joda.time.DateTime;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+
 
 /**
  * Created by ayazlakdawala on 9/7/15.
  */
 public class BaseModelObject {
 
-    private Long id;
+    @Id
+    private String id;
     private DateTime createdDatetime;
 
+    @Version
+    private Long version;
 
-    public Long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
     }
 
     public DateTime getCreatedDatetime() {
