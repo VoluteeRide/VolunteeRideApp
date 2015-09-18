6911b4c8b6d488ffaf6184d98c035124a95a7cb6
ayazlakdawala <ayazlakdawala@gmail.com>
2015-09-18 18:51:06 -0400

    #14 Integrated Jersey with Volunteeride app

---
+++ Volunteeride/service/src/main/java/com/volunteeride/jerseyconfig/JerseyAppConfig.java
@@ -0,0 +1,23 @@
+package com.volunteeride.jerseyconfig;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.inject.Named;
+import javax.ws.rs.ApplicationPath;
+
+/**
+ *
+ * This class is used to register the rest endpoints for Volunteeride Application
+ *
+ * Created by ayazlakdawala on 9/18/2015.
+ */
+@Named
+@ApplicationPath("/volunteeride")
+public class JerseyAppConfig extends ResourceConfig {
+
+    public JerseyAppConfig() {
+        //Register your rest endpoints here
+        packages("com.volunteeride.resource");
+    }
+
+}
---
+++ Volunteeride/service/src/main/java/com/volunteeride/resource/CenterResource.java
@@ -0,0 +1,16 @@
+package com.volunteeride.resource;
+
+import javax.inject.Named;
+import javax.ws.rs.Path;
+
+/**
+ * This file serves as rest endpoint for Center related operation
+ *
+ * Created by ayazlakdawala on 9/18/2015.
+ */
+@Named
+@Path("/centers")
+public class CenterResource {
+
+
+}
