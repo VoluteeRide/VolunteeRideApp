3108eda637b0e8e451ab63aa428f5d28a76373a6
ayazlakdawala <ayazlakdawala@gmail.com>
2015-09-18 18:51:36 -0400

    #7 Added a sample rest webservice to test rest webservice integration.

---
+++ Volunteeride/service/src/main/java/com/volunteeride/resource/RideResource.java
@@ -0,0 +1,39 @@
+package com.volunteeride.resource;
+
+import com.volunteeride.dao.RideDAO;
+import com.volunteeride.model.Ride;
+import com.volunteeride.service.ride.RideService;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+/**
+ *
+ * This file serves as rest endpoint for Ride related operation
+ *
+ * Created by ayazlakdawala on 9/18/2015.
+ */
+@Named
+@Path("/centers/{center_id}/rides")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class RideResource {
+
+    @Inject
+    private RideService rideService;
+
+    @Inject
+    private RideDAO rideDAO;
+
+    @GET
+    public List<Ride> findAll(){
+        return rideDAO.findAll();
+    }
+
+}
