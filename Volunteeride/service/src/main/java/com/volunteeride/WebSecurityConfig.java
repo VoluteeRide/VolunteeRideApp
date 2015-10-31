44536fa6356a26a411b9ab550535393a0a9a40a9
Ayaz Lakdawala <ayazlakdawala@gmail.com>
2015-10-31 17:22:20 -0400

    #25 temporarily permitting access to all urls.

--- Volunteeride/service/src/main/java/com/volunteeride/WebSecurityConfig.java
+++ Volunteeride/service/src/main/java/com/volunteeride/WebSecurityConfig.java
@@ -1,20 +1,21 @@
 package com.volunteeride;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by ayazlakdawala on 10/30/2015.
  */
-//@Configuration
-//@EnableWebSecurity
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/volunteeride/**").access("hasAnyRole('VOLUNTEER', 'RIDE_SEEKER')");
+                .anyRequest().permitAll();
+                //.antMatchers("/volunteeride/**").access("hasAnyRole('VOLUNTEER', 'RIDE_SEEKER')");
     }
 
 }
